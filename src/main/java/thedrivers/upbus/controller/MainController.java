16c227f9cc2c9d596abfdd9204f6d06cff5bf41e
Okgok <yule0079@gmail.com>
2021-09-15 10:37:44 +0900

    [2팀] 기본 템플릿 및 그룹아이디,아티팩트,패키지명 설정 , 깡통패키지,파일설정

---
+++ src/main/java/thedrivers/upbus/controller/MainController.java
@@ -0,0 +1,19 @@
+package thedrivers.upbus.controller;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	
+
+	@GetMapping("/")
+	public String main(Model model) {
+		model.addAttribute("title", "UPCYCLING PROJECT");
+		model.addAttribute("h1text", "page configuration example");
+		return "main";
+	}
+}
