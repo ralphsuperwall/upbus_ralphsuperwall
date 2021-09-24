e3a59b92a63d04526ec8bb2d4fad1b28201918aa
ECS <ECS@DESKTOP-048HU3O>
2021-09-24 12:31:46 +0900

    [2팀](차현송 0924) 각 기능 페이지(관리자) 생성

--- src/main/java/thedrivers/upbus/controller/AdminController.java
+++ src/main/java/thedrivers/upbus/controller/AdminController.java
@@ -1,9 +1,28 @@
 package thedrivers.upbus.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/admin")
 @Controller
 public class AdminController {
-
+	
+	String pageType = "admin";
+	
+	@GetMapping("/standard/MileageManage")
+	public String mileageManage(Model model) {
+		model.addAttribute("title", "UPBUS");
+		model.addAttribute("h1text", "마일리지 등급 기준 관리");
+		return pageType+"/standard/MileageManage";
+	}
+	
+	@GetMapping("/standard/ExpManage")
+	public String expManage(Model model) {
+		model.addAttribute("title", "UPBUS");
+		model.addAttribute("h1text", "경험치 기준관리");
+		return pageType+"/standard/ExpManage";
+	}
 	
 }
