c74f1f18b888aaa916036f0d2daa061bb16bd640
Okgok <yule0079@gmail.com>
2021-09-27 10:53:42 +0900

    [2팀](차현송 0927) 멤버 dto 필드 이름을 카멜 표기법으로 변경

--- src/main/java/thedrivers/upbus/domain/Member.java
+++ src/main/java/thedrivers/upbus/domain/Member.java
@@ -1,136 +1,137 @@
 package thedrivers.upbus.domain;
 
 public class Member {
-	private String member_id;
-	private String member_pw;
-	private String member_name;
-	private String member_email;
-	private String member_gender;
-	private String member_addr;
-	private String member_phone_number;
-	private String member_birth;
-	private String member_mile;
-	private String member_sign;
-	private String grade_code;
-	private String member_exp;
-	private String member_status;
-	private String member_image;
-	private String class_job;
-	private String member_login_history_code;
-	private String member_division;
+	private String memberId;
+	private String memberIw;
+	private String memberName;
+	private String memberEmail;
+	private String memberGender;
+	private String memberAddr;
+	private String memberPhone_number;
+	private String memberBirth;
+	private String memberMile;
+	private String memberSign;
+	private String gradeCode;
+	private String memberExp;
+	private String memberStatus;
+	private String memberSmage;
+	private String classJob;
+	private String memberLogin_history_code;
+	private String memberDivision;
 	
-	public String getMember_id() {
-		return member_id;
+	public String getMemberId() {
+		return memberId;
 	}
-	public void setMember_id(String member_id) {
-		this.member_id = member_id;
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
 	}
-	public String getMember_pw() {
-		return member_pw;
+	public String getMemberIw() {
+		return memberIw;
 	}
-	public void setMember_pw(String member_pw) {
-		this.member_pw = member_pw;
+	public void setMemberIw(String memberIw) {
+		this.memberIw = memberIw;
 	}
-	public String getMember_name() {
-		return member_name;
+	public String getMemberName() {
+		return memberName;
 	}
-	public void setMember_name(String member_name) {
-		this.member_name = member_name;
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
 	}
-	public String getMember_email() {
-		return member_email;
+	public String getMemberEmail() {
+		return memberEmail;
 	}
-	public void setMember_email(String member_email) {
-		this.member_email = member_email;
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
 	}
-	public String getMember_gender() {
-		return member_gender;
+	public String getMemberGender() {
+		return memberGender;
 	}
-	public void setMember_gender(String member_gender) {
-		this.member_gender = member_gender;
+	public void setMemberGender(String memberGender) {
+		this.memberGender = memberGender;
 	}
-	public String getMember_addr() {
-		return member_addr;
+	public String getMemberAddr() {
+		return memberAddr;
 	}
-	public void setMember_addr(String member_addr) {
-		this.member_addr = member_addr;
+	public void setMemberAddr(String memberAddr) {
+		this.memberAddr = memberAddr;
 	}
-	public String getMember_phone_number() {
-		return member_phone_number;
+	public String getMemberPhone_number() {
+		return memberPhone_number;
 	}
-	public void setMember_phone_number(String member_phone_number) {
-		this.member_phone_number = member_phone_number;
+	public void setMemberPhone_number(String memberPhone_number) {
+		this.memberPhone_number = memberPhone_number;
 	}
-	public String getMember_birth() {
-		return member_birth;
+	public String getMemberBirth() {
+		return memberBirth;
 	}
-	public void setMember_birth(String member_birth) {
-		this.member_birth = member_birth;
+	public void setMemberBirth(String memberBirth) {
+		this.memberBirth = memberBirth;
 	}
-	public String getMember_mile() {
-		return member_mile;
+	public String getMemberMile() {
+		return memberMile;
 	}
-	public void setMember_mile(String member_mile) {
-		this.member_mile = member_mile;
+	public void setMemberMile(String memberMile) {
+		this.memberMile = memberMile;
 	}
-	public String getMember_sign() {
-		return member_sign;
+	public String getMemberSign() {
+		return memberSign;
 	}
-	public void setMember_sign(String member_sign) {
-		this.member_sign = member_sign;
+	public void setMemberSign(String memberSign) {
+		this.memberSign = memberSign;
 	}
-	public String getGrade_code() {
-		return grade_code;
+	public String getGradeCode() {
+		return gradeCode;
 	}
-	public void setGrade_code(String grade_code) {
-		this.grade_code = grade_code;
+	public void setGradeCode(String gradeCode) {
+		this.gradeCode = gradeCode;
 	}
-	public String getMember_exp() {
-		return member_exp;
+	public String getMemberExp() {
+		return memberExp;
 	}
-	public void setMember_exp(String member_exp) {
-		this.member_exp = member_exp;
+	public void setMemberExp(String memberExp) {
+		this.memberExp = memberExp;
 	}
-	public String getMember_status() {
-		return member_status;
+	public String getMemberStatus() {
+		return memberStatus;
 	}
-	public void setMember_status(String member_status) {
-		this.member_status = member_status;
+	public void setMemberStatus(String memberStatus) {
+		this.memberStatus = memberStatus;
 	}
-	public String getMember_image() {
-		return member_image;
+	public String getMemberSmage() {
+		return memberSmage;
 	}
-	public void setMember_image(String member_image) {
-		this.member_image = member_image;
+	public void setMemberSmage(String memberSmage) {
+		this.memberSmage = memberSmage;
 	}
-	public String getClass_job() {
-		return class_job;
+	public String getClassJob() {
+		return classJob;
 	}
-	public void setClass_job(String class_job) {
-		this.class_job = class_job;
+	public void setClassJob(String classJob) {
+		this.classJob = classJob;
 	}
-	public String getMember_login_history_code() {
-		return member_login_history_code;
+	public String getMemberLogin_history_code() {
+		return memberLogin_history_code;
 	}
-	public void setMember_login_history_code(String member_login_history_code) {
-		this.member_login_history_code = member_login_history_code;
+	public void setMemberLogin_history_code(String memberLogin_history_code) {
+		this.memberLogin_history_code = memberLogin_history_code;
 	}
-	public String getMember_division() {
-		return member_division;
+	public String getMemberDivision() {
+		return memberDivision;
 	}
-	public void setMember_division(String member_division) {
-		this.member_division = member_division;
+	public void setMemberDivision(String memberDivision) {
+		this.memberDivision = memberDivision;
 	}
 	@Override
 	public String toString() {
-		return "Member [member_id=" + member_id + ", member_pw=" + member_pw + ", member_name=" + member_name
-				+ ", member_email=" + member_email + ", member_gender=" + member_gender + ", member_addr=" + member_addr
-				+ ", member_phone_number=" + member_phone_number + ", member_birth=" + member_birth + ", member_mile="
-				+ member_mile + ", member_sign=" + member_sign + ", grade_code=" + grade_code + ", member_exp="
-				+ member_exp + ", member_status=" + member_status + ", member_image=" + member_image + ", class_job="
-				+ class_job + ", member_login_history_code=" + member_login_history_code + ", member_division="
-				+ member_division + "]";
+		return "Member [memberId=" + memberId + ", memberIw=" + memberIw + ", memberName=" + memberName
+				+ ", memberEmail=" + memberEmail + ", memberGender=" + memberGender + ", memberAddr=" + memberAddr
+				+ ", memberPhone_number=" + memberPhone_number + ", memberBirth=" + memberBirth + ", memberMile="
+				+ memberMile + ", memberSign=" + memberSign + ", gradeCode=" + gradeCode + ", memberExp=" + memberExp
+				+ ", memberStatus=" + memberStatus + ", memberSmage=" + memberSmage + ", classJob=" + classJob
+				+ ", memberLogin_history_code=" + memberLogin_history_code + ", memberDivision=" + memberDivision + "]";
 	}
+	
+
 	
 	
 }
