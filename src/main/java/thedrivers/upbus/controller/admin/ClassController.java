3fd6b40d347eaf00867553797f79a360a10a5189
USER <USER@DESKTOP-LKCFTFK>
2021-10-10 12:39:02 +0900

    [ 수정사항 ]

--- src/main/java/thedrivers/upbus/controller/admin/ClassController.java
+++ src/main/java/thedrivers/upbus/controller/admin/ClassController.java
@@ -1,15 +1,60 @@
 package thedrivers.upbus.controller.admin;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import thedrivers.upbus.domain.ClassAddTeacherList;
+import thedrivers.upbus.service.ClassService;
 
 @RequestMapping("/admin/class")
 @Controller
 public class ClassController {
 	
 	String pageType = "admin/class";
+	
+	/**
+	 * 메소드 주입 방식 (service)
+	 */
+	
+	private final ClassService classService;
+	
+	public ClassController(ClassService classService) {
+		this.classService = classService;
+	}
+	
+	
+	// (1) classAddTeacherManage : 클래스 강사 신청을 관리자가 받고 승인하도록 하는 화면
+	@GetMapping("/ClassAddTeacherManage")
+	public String classAddTeacherManage(Model model) {
+		model.addAttribute("title", "클래스강사신청관리");
+		
+		// 클래스 강사 신청( class_teacher_signup 테이블 )에서 조회된 리스트들을 가져온다.
+		List<ClassAddTeacherList> classAddTeacherList = classService.getClassAddTeacherList();
+		
+		model.addAttribute("classAddTeacherList", classAddTeacherList);
+		
+		return  pageType+"/ClassAddTeacherManage";
+	}
+	
+	
+	// (2) classAddTeacherManage : 클래스 강사 신청 화면에서 클래스 기간 승인에 대한 버튼에 대한 AJAX 를 사용한다.
+	@PostMapping(value ="/TermApproveCheckAjax", produces="appliaction/json")
+	@ResponseBody
+	public Map<String, Object> termApproveCheckAjax( Model model
+												   , @RequestParam( value = "TermApproveCheckValue" ) String termApproveCheckValue ){
+		Map<String, Object> map = new HashMap <String, Object>();
+
+		return map;
+	}
 	
 	@GetMapping("/EnrollmentManage")
 	public String enrollmentManage(Model model) {
--- src/main/java/thedrivers/upbus/mapper/ClassMapper.java
+++ src/main/java/thedrivers/upbus/mapper/ClassMapper.java
@@ -1,6 +1,10 @@
 package thedrivers.upbus.mapper;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+
+import thedrivers.upbus.domain.ClassAddTeacherList;
 
 @Mapper
 public interface ClassMapper {
@@ -8,13 +12,19 @@ public interface ClassMapper {
 	/**************************************************************************
 	 * 1.클래스 강사 신청을 할 때 데이터를 받아 처리한다. *
 	 **************************************************************************/
-	public int ClassAddTeacher(	String sessionID
-							  ,	String class_techaer_email
-							  , String class_teacher_name
-							  ,	String class_teacher_team
-							  , String class_teacher_phone_number
-							  ,	String class_teacher_signup_file
-							  , String class_teacher_schedule_file
-							  , String class_teacher_resum_file   ); 
+	public int classAddTeacher(	String sessionID
+							  ,	String classTechaerEmail
+							  , String classTeacherName
+							  ,	String classTeacherTeam
+							  , String classTeacherPhoneNumber
+							  ,	String classTeacherSignupFile
+							  , String classTeacherScheduleFile
+							  , String classTeacherResumFile   ); 
+	
+	/**************************************************************************
+	 * 2.입력값 없이 메소드를 호출하면 class_teacher_signup 테이블의 값을 찾아서 리스트를 가져와야 한다. (반환 : 리스트<도메인>) *
+	 **************************************************************************/
+	public List<ClassAddTeacherList> getClassAddTeacherList();
+	
 
 }
